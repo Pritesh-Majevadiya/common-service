8b44a837342a719976faec9d395d587cae5fefaa
Pritesh <priteshm002@gmail.com>
2025-04-27 15:28:15 +0530

    Build service

--- src/main/java/com/common_service/CommonServiceApplication.java
+++ src/main/java/com/common_service/CommonServiceApplication.java
@@ -1,14 +1,8 @@
 package com.common_service;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CommonServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonServiceApplication.class, args);
 		System.out.println("Common Service started....");
 	}
-
 }
